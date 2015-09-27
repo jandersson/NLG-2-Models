959873af5ca9531d0da3d92a2feac9da91d2ff23
jonas <jonas@andersson.io>
2015-09-27 13:38:23 +0200

    Create make sentences function

--- ngramTrials.py
+++ ngramTrials.py
@@ -6,27 +6,24 @@ from nltk.probability import MLEProbDist
 
 class nGramModel():
 
-	def __init__(self, samples, estimator, n = 2):
-		cfd = ConditionalFreqDist()
-		for sent in samples:
-			sent.insert(0,'<s>')
-			sent.append('</s>')
-		for sent in samples:
-			for (w1,w2) in list(zip(sent[:-1],sent[1:])):
-				cfd[w1][w2] +=1
-		self.model = ConditionalProbDist(cfd, estimator)
+    def __init__(self, samples, estimator, n = 2):
+        cfd = ConditionalFreqDist()
+        for sent in samples:
+            sent.insert(0,'<s>')
+            sent.append('</s>')
+            for sent in samples:
+                for (w1,w2) in list(zip(sent[:-1],sent[1:])):
+                    cfd[w1][w2] +=1
+                self.model = ConditionalProbDist(cfd, estimator)
 
+#sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:2]]
+def make_sentences(corpus, category=None):
+    return [[w.lower() for w in s if w.isalnum()] for s in corpus(categories=category)[:2]]
 
-
-
-sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:2]]
-
-model = nGramModel(sentences,MLEProbDist).model
-
-test = ["<s>","i", "can", "like", "apples","</s>"]
-p = 1
-
-for i in range(len(test)-1):
-	print(test[i],test[i+1])
-	print(model[test[i]].prob(test[i+1]))
-	p *= model[test[i]].prob(test[i+1])
+# test = ["<s>","i", "can", "like", "apples","</s>"]
+# p = 1
+#
+# for i in range(len(test)-1):
+#     print(test[i],test[i+1])
+#     print(model[test[i]].prob(test[i+1]))
+#     p *= model[test[i]].prob(test[i+1])
