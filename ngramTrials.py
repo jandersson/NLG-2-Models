12a9f740c09efe5b6fea2aa7b06714823948c9fd
Jokull Johannsson <jokull.johanns@gmail.com>
2015-09-27 12:02:09 +0200

    Changed to implementation to use class

--- ngramTrials.py
+++ ngramTrials.py
@@ -4,25 +4,27 @@ from nltk.probability import ConditionalFreqDist
 from nltk.probability import ConditionalProbDist
 from nltk.probability import MLEProbDist
 
-cfd = ConditionalFreqDist()
+class nGramModel():
+
+	def __init__(self, samples, estimator, n = 2):
+		cfd = ConditionalFreqDist()
+		for sent in samples:
+			sent.insert(0,'<s>')
+			sent.append('</s>')
+		for sent in s:
+			for (w1,w2) in list(zip(sent[:-1],sent[1:])):
+				cfd[w1][w2] +=1
+		self.model = ConditionalProbDist(cfd, estimator)
+
 
 s = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:2]]
 
-for sent in s:
-	sent.insert(0,'<s>')
-	sent.append('</s>')
-
-for sent in s:
-	print("sent : ",sent)
-	for (w1,w2) in list(zip(sent[:-1],sent[1:])):
-		print("Bigram: ",w1,w2)
-		cfd[w1][w2] +=1
-cpd = ConditionalProbDist(cfd, MLEProbDist)
+model = nGramModel(s,MLEProbDist).model
 
 test = ["<s>","i", "can", "like", "apples","</s>"]
 p = 1
 
 for i in range(len(test)-1):
 	print(test[i],test[i+1])
-	print(cpd[test[i]].prob(test[i+1]))
-	p *= cpd[test[i]].prob(test[i+1])
+	print(model[test[i]].prob(test[i+1]))
+	p *= model[test[i]].prob(test[i+1])
