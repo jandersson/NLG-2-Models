96c566ffeb2e494d7943c5a63ce4655ac36929de
Jokull Johannssonn <jokulljohannsson@SE-MAC-109-OSX.local>
2015-09-27 15:49:29 +0200

    Checked in a wrong file :D

--- ngramTrials.py
+++ ngramTrials.py
@@ -9,17 +9,28 @@ class nGramModel():
 
     def __init__(self, samples, estimator, n = 2):
         cfd = ConditionalFreqDist()
+        for sent in samples:
             sent.insert(0,'<s>')
             sent.append('</s>')
-        for sent in samples:
-            for (w1,w2) in list(nltk.bigrams(sent)):
-                cfd[w1][w2] +=1
+            grams = nltk.ngrams(sent,n)
+            if n == 2:
+                for (w1,w2) in list(grams):
+                    cfd[w1][w2] +=1
+            if n == 3:
+                for (w1,w2,w3) in list(grams):
+                    cfd[w1+ " " +w2][w3] +=1
+            if n == 4:
+                for (w1,w2,w3,w4) in list(grams):
+                    cfd[w1+ " " +w2+ " " +w3][w4] +=1
+            if n == 5:
+                for (w1,w2,w3,w4,w5) in list(grams):
+                    cfd[w1+ " " +w2+ " " +w3+ " " +w4][w5] +=1
         self.model = ConditionalProbDist(cfd, estimator)
 
     def estimateWord(self, word, numberOfEstimates):
         wordEstimates = {}
         for key in self.model[word].freqdist():
-            #print(self.model[word].prob(key))
+            print(key + ": " + repr(self.model[word].prob(key)))
             wordEstimates[key] = self.model[word].prob(key)
         orderedEstimates = ((k, wordEstimates[k]) for k in sorted(wordEstimates, key=wordEstimates.get, reverse=True))
         return [(k,v) for k, v in orderedEstimates][:numberOfEstimates]
@@ -38,11 +49,9 @@ class nGramModel():
         return this.model
 
 sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:10000]]
-adventureModel = nGramModel(sentences,MLEProbDist)
+adventureModel = nGramModel(sentences,MLEProbDist,4)
 
 testSentence = ["<s>","i", "can", "like", "apples","</s>"]
+#print(adventureModel.probSentence(testSentence))
 
-
-
-print(adventureModel.probSentence(testSentence))
-print(adventureModel.estimateWord('like',5))
+print(adventureModel.estimateWord('in the car',5))
