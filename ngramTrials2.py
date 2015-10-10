931bd6208f43bce47ae7e1bf6ed61f7c9d9bb5ff
Jokull Johannssonn <jokulljohannsson@SE-MAC-109-OSX.local>
2015-10-10 16:08:03 +0200

    Working sentence generator that generates awesome sentences :)

--- ngramTrials2.py
+++ ngramTrials2.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.corpus import brown
 from nltk.probability import ConditionalFreqDist
 from nltk.probability import ConditionalProbDist
-from nltk.probability import MLEProbDist
+from  nltk import probability
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -14,12 +14,12 @@ class nGramModel():
     def __init__(self, TaggedSentences, estimator, n = 2):
         self.n = n
         self.models = []
-        print("Starting the training of model")
+        print("Starting the training of the model")
         if n > 1: ##bigrams
 
             self.addPseudo(TaggedSentences,1)
             TaggedTuples = (item for sublist in TaggedSentences for item in sublist)
-            TaggedTuples = [(t1.lower(), t2) for t1, t2 in TaggedTuples if (t1.isalnum() or t1 in("<s>","</s>"))]
+            TaggedTuples = [(t1.lower(), t2) for t1, t2 in TaggedTuples if (t1.isalpha() or t1 in("<s>","</s>"))]
             biGrams = nltk.ngrams(TaggedTuples,2)
             cfdBiGrams = ConditionalFreqDist()
             for (gram1,gram2) in list(biGrams):
@@ -30,7 +30,7 @@ class nGramModel():
         if n > 2: #triGrams
             self.addPseudo(TaggedSentences,1)
             trigram_tuples = (item for sublist in TaggedSentences for item in sublist)
-            tagged_trigram_tuples = [(t1.lower(), t2) for t1, t2 in trigram_tuples if (t1.isalnum() or t1 in("<s>","</s>"))]
+            tagged_trigram_tuples = [(t1.lower(), t2) for t1, t2 in trigram_tuples if (t1.isalpha() or t1 in("<s>","</s>"))]
             triGrams = nltk.ngrams(tagged_trigram_tuples,3)
             cfdTriGrams = ConditionalFreqDist()
             for (gram1,gram2,gram3) in list(triGrams):
@@ -41,7 +41,7 @@ class nGramModel():
         if n > 3: #quadGrams
             self.addPseudo(TaggedSentences,1)
             TaggedTuples = (item for sublist in TaggedSentences for item in sublist)
-            TaggedTuples = [(t1.lower(), t2) for t1, t2 in TaggedTuples if (t1.isalnum() or t1 in("<s>","</s>"))]
+            TaggedTuples = [(t1.lower(), t2) for t1, t2 in TaggedTuples if (t1.isalpha() or t1 in("<s>","</s>"))]
             quadGrams = nltk.ngrams(TaggedTuples,4)
             cfdQuadGrams = ConditionalFreqDist()
             for (gram1,gram2,gram3,gram4) in list(quadGrams):
@@ -57,17 +57,15 @@ class nGramModel():
                 sent.append(("</s>","</s>"))
 
     def generate(self,context):
-        print("Finding the most probable word given a certain context")
         return self.generateRec(context,self.n)[0]
 
     def generateRec(self, context,curNGram):
         if curNGram == 1:
-            return ("","")
+            return ("</s>","</s>")
         tokenized = nltk.word_tokenize(context.lower())
         tagged = nltk.pos_tag(tokenized, tagset='universal')
-
         if len(tagged) < curNGram:
-            for i in range(curNGram -len(tagged)-1):
+            for i in range(curNGram -len(tagged)):
                 tagged.insert(0, ("<s>","<s>"))
         tagged = tagged[-(curNGram - 1):]
         grams =  list(nltk.ngrams(tagged, curNGram-1))[0][0] if curNGram == 2  else list(nltk.ngrams(tagged, curNGram-1))[0]
@@ -77,46 +75,38 @@ class nGramModel():
             try:
                 return self.generateRec(context, curNGram-1)
             except:
-                return ""
-        
+                return "</s>"
 
+    def createSentences(self,count = 1):
+        for i in range(count):
+            text = ""
+            tk = ""
+            while(tk != "</s>"):
+                text += tk + " "
+                tk = self.generate(text.strip())
+            print(text.strip().capitalize()+".")
 
-print("Loading the corpus")
-TaggedCorpus = [w for w in brown.tagged_sents(tagset='universal')]
-model = nGramModel(TaggedCorpus, MLEProbDist, 4)
-# print(model.generate("problem"))
-model.generate("the man who won the man")
-model.generate("blarg")
-model.generate("horse")
+def printTags(sent):
+    tokenized = nltk.word_tokenize(sent.lower())
+    print(tokenized)
+    tagged = nltk.pos_tag(tokenized, tagset='universal')
+    print(tagged)
 
-print("Starting to generate the sentence")
-text = ""
-prevTk = "the"
-tk = ""
-while(tk != "</s>"):
-    text += tk + " "
-    tk = model.generate(prevTk)
-    prevTk = tk
-print(text.strip())
+def generateSentenceFromModel(model, smoothingName,count):
+    print("-----------------Starting sentence generations-----------------")
+    print("GramCount:"+ str(model.n));
+    print("Smoothing:"+ smoothingName);
+    model.createSentences(count)
+    print("-----------------Ending sentence generations-----------------")
 
+TaggedCorpus = [w for w in brown.tagged_sents(tagset='universal')[:1000]]
+model1 = nGramModel(TaggedCorpus, probability.MLEProbDist, 4)
+#model3 = nGramModel(TaggedCorpus, probability.LaplaceProbDist, 4)
+#model4 = nGramModel(TaggedCorpus, probability.ELEProbDist, 4)
+#model7 = nGramModel(TaggedCorpus, probability.SimpleGoodTuringProbDist, 4)
 
+generateSentenceFromModel(model1,"MLEProbDist",20)
+#generateSentenceFromModel(model3,"LaplaceProbDist",20)
+#generateSentenceFromModel(model4,"ELEProbDist",20)
+#generateSentenceFromModel(model7,"SimpleGoodTuringProbDist",20)
 
-# ('in', 'IN'), ('the', 'AT')
-# ('which', 'WDT'), ('was', 'BEDZ')
-
-      #
-      # if source == 'wsj':
-      #       source = 'en-ptb'
-      #   if source == 'brown':
-      #       source = 'en-brown'
-
-
-'''
-sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:10000]]
-adventureModel = nGramModel(sentences,MLEProbDist,4)
-
-testSentence = ["<s>","i", "can", "like", "apples","</s>"]
-#print(adventureModel.probSentence(testSentence))
-
-print(adventureModel.estimateWord('in the car',5))
-'''
