af03a9fbcadbe51089ead565d76e819d52e2f61f
Edvard Ahlsén <edvard@Edvards-MacBook-Pro.local>
2015-10-11 11:15:36 +0200

    last weekends code

--- main.py
+++ main.py
@@ -4,33 +4,20 @@ from nltk.probability import ELEProbDist
 from math import log
 
 def main():
-    sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')]
-    rSentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='religion')]
-
-    addPseudo(rSentences)
-    addPseudo(sentences) # Add pseudowords
-    split = 9*len(sentences)//10
-    train = sentences[:split]
-    test = sentences[split:]
-
-
-    test2 = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='religion')][:len(test)]
-    addPseudo(test2) # Add pseudowords
-
-    # Train models
-    adventureModel = nGramModel(train,ELEProbDist)
-    religionModel = nGramModel(rSentences,ELEProbDist)
-
-    p = perplexity(adventureModel,test)
-    p2 = perplexity(adventureModel,test2)
+    testModel = generateModelFromSentences(brown.sents(categories='adventure'), ELEProbDist, 3)
+    #p = perplexity(adventureModel,test)
+    #p2 = perplexity(adventureModel,test2)
 
     # Generate cool shit
-    generateText(adventureModel, 5)
-    print()
-    generateText(religionModel, 5)
+    generateText(testModel, 5)
+    #generateText(religionModel, 5)
     #print(p, p2)
 
 
+def generateModelFromSentences(sents, smoothingF, n):
+    sents = [[w.lower() for w in s if w.isalnum()] for s in sents]
+    addPseudo(sents,n)
+    return nGramModel(sents, smoothingF, n)
 
 def entropy(model, test):
     p = 0
@@ -47,21 +34,28 @@ def perplexity(model, test):
     return 2**e
 
 #mutator
-def addPseudo(sents):
+def addPseudo(sents, n):
     for s in sents:
-        s.insert(0,'<s>')
-        s.append('</s>')
+        for _ in range(n-1):
+            s.insert(0,'<s>')    
+            s.append('</s>')
 
 def generateText(model, sents):
+    prevTk = list()
+    lN = model.getOrder()
     for _ in range(sents):
         text = ""
-        prevTk = "<s>"
+        for _ in range(lN-1):
+            prevTk.append("<s>")
         tk = ""
         while(tk != "</s>"):
             text += tk + " "
-            tk = model.generate(prevTk)
-            prevTk = tk
+            tk = model.generate(list(prevTk))
+            prevTk.pop(0)
+            prevTk.append(tk)
+
         print(text.strip())
+        prevTk = list()
 
 if __name__ == '__main__':
     main()
--- ngramTrials.py
+++ ngramTrials.py
@@ -6,24 +6,90 @@ from nltk.probability import MLEProbDist
 import collections
 
 class nGramModel():
-
-
     
     def __init__(self, samples, estimator, n = 2):
-        cfd = ConditionalFreqDist()
+        self.order = n
+        print("my order: " + str(self.order))
+
+        self.grams = list()
         for sent in samples:
-            grams = nltk.ngrams(sent,n)
-            if n == 2:
-                for (w1,w2) in list(grams):
-                    cfd[w1][w2] +=1
-            if n == 3:
-                for (w1,w2,w3) in list(grams):
-                    # This is what we want: cfd[(w1,w2)][w3] +=1
-                    cfd[w1+ " " +w2][w3] +=1
-            if n == 4:
-                for (w1,w2,w3,w4) in list(grams):
-                    cfd[w1+ " " +w2+ " " +w3][w4] +=1
-        self.model = ConditionalProbDist(cfd, estimator)
+            self.grams += list(nltk.ngrams(sent,n)) # get iterator over ngrams
+        
+        #print(self.grams[:100])
+
+        self.countNgrams(self.grams, n, estimator) # sets self.model
+
+        print("NO ERRORS!!!!")
+
+        '''
+        if n == 2:
+            for (w1,w2) in list(grams):
+                cfd[w1][w2] +=1
+        
+
+        if n == 3:
+            for (w1,w2,w3) in list(grams):
+                cfd[(w1,w2)][w3] +=1
+                cfd[w1][w2] [w3] += 1
+
+                #cfd[w1+ " " +w2][w3] +=1
+        if n == 4:
+            for (w1,w2,w3,w4) in list(grams):
+
+                cfd[(w1,w2,w3)][w4]
+
+                cfd[w1+ " " +w2+ " " +w3][w4] +=1
+        '''
+
+        #self.model = ConditionalProbDist(cfd, estimator)
+
+
+    def countNgrams(self, grams, n, smoothingF):
+        if (n > 2):
+            self.model = rec_dd()
+        else:
+            self.model = ConditionalFreqDist()
+
+        for item in list(grams):
+            item = list(item)
+            # For each new gram
+            thisDict = self.model  # point to top-level dictionary 
+            thisN = n # reset n-counter
+
+            while(thisN >= 2):
+                if( thisN > 3):
+                    wn = item.pop(0) # pop first word in gram
+                    thisDict = thisDict[wn] ## returns dict.
+                    thisN -= 1
+
+                elif (thisN == 3):
+                    w3 = item.pop(0)
+                    if w3 not in thisDict:
+                        thisDict[w3] = ConditionalFreqDist()    
+                    
+                    thisDict = thisDict[w3]
+                    thisN -= 1
+
+                elif (thisN == 2):
+                    w2 = item.pop(0)
+                    thisDict[w2][item.pop(0)] += 1
+                    thisN -= 1
+
+        self.probNGrams(n,self.model,smoothingF)
+        return
+
+    def probNGrams(self, n, dic, smoothingF):
+        if (n == 2): # Special for bigrams
+            self.model = ConditionalProbDist(dic, smoothingF)
+            return
+
+        else:
+            for (k,v) in dic.items():
+                if (n == 3):
+                    dic[k] = ConditionalProbDist(v, smoothingF)
+                else:
+                    self.probNGrams(n-1, v, smoothingF)
+        return
 
     def estimateWord(self, word, numberOfEstimates):
         wordEstimates = {}
@@ -36,8 +102,18 @@ class nGramModel():
     def prob(self, word, previous):
         return self.model[previous].prob(word)
 
-    def generate(self, word):
-        return self.model[word].generate()
+    def generate(self, words):
+        order = self.order
+        dictionary = self.model
+        while (order > 2):
+            if (words[0] not in dictionary):
+                print("uh oh, back off!")
+                return
+            dictionary = dictionary[words.pop(0)]
+            order -= 1
+
+        probdist = dictionary[words.pop(0)]
+        return probdist.generate()
 
     def probSentence(self, sentence):
         prob = 1
@@ -46,6 +122,12 @@ class nGramModel():
             prob *= self.model[sentence[i]].prob(sentence[i+1])
         return prob
 
+    def getOrder(self):
+        return self.order
+
+
+def rec_dd():
+    return collections.defaultdict(rec_dd)
 '''
 sentences = [[w.lower() for w in s if w.isalnum()] for s in brown.sents(categories='adventure')[:10000]]
 adventureModel = nGramModel(sentences,MLEProbDist,4)
